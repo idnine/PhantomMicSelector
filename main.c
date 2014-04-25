f0a22404665b6e6b3b94bb64fb8f00fb9e2ab9e0
Koo Jonghoe <idnine@gmail.com>
2014-04-25 15:04:34 +0900

    MSP430 Launchpad LED Blink Test

--- main.c
+++ main.c
@@ -6,5 +6,15 @@
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 	
+    P1DIR |= 0x01;
+
+    volatile unsigned int i;
+
+    while(1) {
+    	P1OUT = 0x01;
+    	for(i=0; i <= 20000; i++);
+    	P1OUT = 0x00;
+    	for(i=0; i <= 20000; i++);
+    }
 	return 0;
 }
